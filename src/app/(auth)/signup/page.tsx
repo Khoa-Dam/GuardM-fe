3c8335d00cf2f1342c9de65fa822ad1bc32ab974
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-22 23:09:53 +0700

    Add Spinner

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react"
 import { BlurFade } from "@/components/blur-fade"
 import { SignupForm } from "../components/signup-form"
+import { Spinner } from "@/components/ui/spinner"
 
 export default function SignupPage() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center bg-zinc-900 gap-6 p-6 md:p-10">
             <BlurFade delay={0.25} inView>
                 <div className="flex w-full max-w-sm flex-col gap-6">
-                    <Suspense fallback={<div>Loading...</div>}>
+                    <Suspense fallback={<Spinner className="size-10 animate-spin" />}>
                         <SignupForm />
                     </Suspense>
                 </div>
